f2ae4eb201a07d9cd1add8e4d26830035351ea4d
Jane <spring1981@mail.ru>
2023-08-31 13:06:20 +0300

    homework created

---
+++ homework_1.js
@@ -0,0 +1,27 @@
+// Домашня работа по итогам урока 1
+
+/*
+let newName = "Eva";
+console.log("Имя: " + newName);
+
+let newAge = 35;
+console.log("Возраст: " + newAge);
+
+let marriedStatus = 'женат';
+if (true) {
+    console.log("Есть супруг/супруга: " + marriedStatus);
+}
+
+*/
+
+/*
+let newName = "Eva";
+console.log("Тип первой переменной: " + typeof newName);
+
+let newAge = 35;
+console.log("Тип второй переменной: " + typeof newAge);
+
+let marriedStatus = true;
+console.log("Тип второй переменной: " + typeof marriedStatus);
+
+*/
