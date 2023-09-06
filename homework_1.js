3d90e440a81b0ffb128c6a9208f786b6996eead0
Jane <spring1981@mail.ru>
2023-09-06 08:04:03 +0300

    hw2 complited

--- homework_1.js
+++ homework_1.js
@@ -1,18 +1,17 @@
-// Домашня работа по итогам урока 1
+// Домашняя работа по итогам урока 1
 
 /*
 let newName = "Eva";
-console.log("Имя: " + newName);
+console.log(`Имя: ${newName}`);
 
 let newAge = 35;
-console.log("Возраст: " + newAge);
+console.log(`Возраст: ${newAge}`);
 
-let marriedStatus = 'женат';
-if (true) {
-    console.log("Есть супруг/супруга: " + marriedStatus);
-}
+let marriedStatus = true;
+console.log(`Есть супруг/супруга: ${marriedStatus}`);
 
 */
+
 
 /*
 let newName = "Eva";
@@ -23,5 +22,40 @@ console.log("Тип второй переменной: " + typeof newAge);
 
 let marriedStatus = true;
 console.log("Тип второй переменной: " + typeof marriedStatus);
+
+*/
+
+// Домашняя работа по итогам урока 2
+
+/*
+let hour = 1;
+let sec = (hour*60*60);
+
+console.log(`В часе ${sec} секунд`);
+
+*/
+
+/*
+let number = 0;
+
+number += 7;
+number *= 4;
+number -= 8;
+number /= 4;
+number **= 3;
+let lastNumber = number % 5;
+
+console.log(lastNumber);
+
+*/
+
+/*
+console.log(5 > 4);
+console.log("ананас" > "яблоко");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "0");
+console.log(null === +"0");
 
 */
